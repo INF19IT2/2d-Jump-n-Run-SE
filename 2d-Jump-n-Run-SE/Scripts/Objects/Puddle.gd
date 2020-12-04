cdf55056a4400d615ad10c2113e3db3e625cf59f
cedimo <johanneslehm@gmail.com>
2020-12-04 11:20:16 +0100

    puddle deals damage in intervals

--- 2d-Jump-n-Run-SE/Scripts/Objects/Puddle.gd
+++ 2d-Jump-n-Run-SE/Scripts/Objects/Puddle.gd
@@ -3,6 +3,13 @@ extends Area2D
 var overlapsPlayer = false
 var player
 
+# damage interval in s
+var damageTime = 1
+# damage value
+var damageValue = 5
+
+var temp : float = damageTime
+
 func _on_Puddle_body_entered(body):
 	if body.has_method('get_name'):
 		if body.get_name() == "Player":
@@ -17,4 +24,7 @@ func _on_Puddle_body_exited(body):
 func _process(delta):
 	if overlapsPlayer && player != null:
 		if player.has_method('take_damage'):
-			player.take_damage(delta)
+			temp += delta
+			if temp >= damageTime:
+				player.take_damage(damageValue)
+				temp = 0
