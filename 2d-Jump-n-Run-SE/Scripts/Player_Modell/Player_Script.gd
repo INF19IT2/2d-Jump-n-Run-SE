c0e77b884ced79c62db5ae3bb6510b124a7a05df
DerNoob24 <74051636+DerNoob24@users.noreply.github.com>
2021-04-26 11:20:04 +0200

    moved files, renamed directory and added signal
    moved checkpoint script to script directory, renamed mainscene directory, and added player dead signal if player dies

--- 2d-Jump-n-Run-SE/Scripts/Player_Modell/Player_Script.gd
+++ 2d-Jump-n-Run-SE/Scripts/Player_Modell/Player_Script.gd
@@ -48,7 +48,6 @@ func _ready():
 func set_checkpoint(var check):
 	pos = check
 
-
 func respawn():
 	self.position = pos
 	health = health_max
@@ -59,7 +58,7 @@ func respawn():
 func _physics_process(delta):
 	velocity.x = 0
 	is_moving = false
-	
+
 	#define movement
 	if Input.is_action_pressed("move_right"):
 		velocity.x += speed
@@ -71,7 +70,7 @@ func _physics_process(delta):
 
 		is_moving = true
 
-	
+
 	#create projectile on player position
 	if Input.is_action_just_pressed("shoot") and is_ducked == false:
 		if projectileCooldown <= 0.0:
@@ -80,17 +79,17 @@ func _physics_process(delta):
 			get_parent().add_child(projectile)
 			projectileCooldown = PROJECTILE_COOLDOWN_TIME
 	projectileCooldown -= delta
-	
+
 	if is_on_floor():
 		player_jump.visible = false
 		player_standing.visible = true
 	elif not is_on_floor() and not is_ducked:
 		player_standing.visible = false
 		player_jump.visible = true
-		
+
 	#gravity
 	velocity.y += gravity * delta
-	
+
 	if velocity.x != 0:
 		player_standing.flip_v = false
 		player_standing.flip_h = velocity.x < 0
@@ -98,13 +97,13 @@ func _physics_process(delta):
 		player_ducked.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		player_jump.flip_v = velocity.y > 0
-		
+
 	#define Jumping
 	if Input.is_action_just_pressed("move_up") and is_on_floor() and !is_ducked:
 		velocity.y -= jumpForce
 		player_jump.visible = true
 		player_standing.visible = false
-		
+
 	#define running
 	if Input.is_action_pressed("running") and is_moving and !is_ducked:
 		if(facingRight):
@@ -112,7 +111,7 @@ func _physics_process(delta):
 		elif(!facingRight):
 			velocity.x -= speed
 		
-	
+
 	#var player_pos = player.get_position_in_parent()
 
 	if Input.is_action_pressed("move_down") and is_on_floor():
@@ -121,32 +120,33 @@ func _physics_process(delta):
 		player_ducked.visible = true
 		player_standing.visible = false
 		is_ducked=true
-	
+
 	if not Input.is_action_pressed("move_down"):
 		standing_collision.disabled = false
 		crouching_collision.disabled = true
 		is_ducked = false
 		player_standing.visible = true
 		player_ducked.visible = false
-	
+
 	#Applying Velocity
 		#Vector2.UP -> Ground is facing up
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 	if Input.is_action_just_pressed("test_dmg"):
 		take_damage(10)
-		
+
 	if get_position().floor().y > 600:
 		take_damage(5)
-		
+
 func take_damage(dmg):
 	if health > dmg:
 		health -= dmg
 		emit_signal("player_stats_changed", self)
 	else:
+		emit_signal("player_dead")
 		print("Dead")
 		respawn()
-	
+
 func add_collectable():
 	coins += 1
 	var lab = get_node("Label")
