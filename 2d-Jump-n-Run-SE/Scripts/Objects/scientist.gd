256fb614205380ec396a58ba11804223f95994b3
DerNoob24 <74051636+DerNoob24@users.noreply.github.com>
2021-05-28 11:30:38 +0200

    scientist slidebale aswell as projectiles

--- 2d-Jump-n-Run-SE/Scripts/Objects/scientist.gd
+++ 2d-Jump-n-Run-SE/Scripts/Objects/scientist.gd
@@ -59,6 +59,7 @@ func _physics_process(delta):
 		var projectile = PROJECTILE.instance()
 		projectile.start(position, facingRight)
 		get_parent().add_child(projectile)
+		projectile.set_slider_values()
 		projectileCooldown = PROJECTILE_COOLDOWN_TIME
 	
 	#
