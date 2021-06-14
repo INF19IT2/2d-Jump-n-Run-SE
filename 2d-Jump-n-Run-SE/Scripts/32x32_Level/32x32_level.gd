fd520e21a4072db2d9584bd9500b64b34d2f6af9
reusdomi <domi_re@web.de>
2021-06-14 09:02:39 +0200

    Puddle spawn fixed

--- 2d-Jump-n-Run-SE/Scripts/32x32_Level/32x32_level.gd
+++ 2d-Jump-n-Run-SE/Scripts/32x32_Level/32x32_level.gd
@@ -1,31 +1,11 @@
 extends Node2D
 
-signal projectile_speed_slider_changed
-signal projectile_size_slider_changed
-
 const PUDDLE = preload("res://Scenes/Objects/Puddle.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var slider = get_node("CanvasLayer/Virus/Control")
-	slider.connect("dummy_slider_changed", self, "changed_dummy_size")
-	slider.connect("projectile_speed_slider_changed", self, "changed_projectile_speed")
-	slider.connect("projectile_size_slider_changed", self, "changed_projectile_size")
 	var virus = get_node("CanvasLayer/Virus")
 	virus.connect("spam",self,"spawnpuddle")
-	
-
-func changed_dummy_size(value):
-	var dummy = get_node("KinematicBody2D")
-	dummy.scale = Vector2(value, value)
-	var scientist = get_node("scientist")
-	scientist.scale = Vector2(value, value)
-	
-func changed_projectile_speed(value):
-	emit_signal("projectile_speed_slider_changed", value)
-
-func changed_projectile_size(value):
-	emit_signal("projectile_size_slider_changed", value)
 
 func spawnpuddle(pos):
 	var puddle = PUDDLE.instance()
